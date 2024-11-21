2c73b8615e02a2983f6badd54ad85a8e0a9ed720
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2024-11-21 16:51:23 +0200

    jhahahahahahahahahahahahahahah

--- main.py
+++ main.py
@@ -7,8 +7,32 @@ app = Flask(__name__)
 def index():
     atbilde = requests.get("https://api.chucknorris.io/jokes/random")
     joks = atbilde.json()
+    atbilde = requests.get("https://api.chucknorris.io/jokes/categories")
+    kategorijas = atbilde.json()
 
-    return render_template("index.html", joks = joks["value"], bilde = joks["icon_url"])
+    if request.method == "POST":
+        kategorija = request.form["kat"]
+        atbilde = requests.get(f"https://api.chucknorris.io/jokes/random?category={kategorija}")
+        joks = atbilde.json()
+
+    return render_template("index.html", joks = joks["value"], bilde = joks["icon_url"],kategorijas = kategorijas)
+
+@app.route("/uni")
+def uni():
+    atbilde = requests.get("http://universities.hipolabs.com/search?country=latvia")
+    visas = atbilde.json()
+    nosaukumi = []
+    print(visas[0]["web_pages"][0]) #izprintē tikai 
+    for elements in visas:
+        pieliekamais = {
+            "nosaukums": elements["name"],
+            "majaslapa": elements["web_pages"]
+        
+        }
+        nosaukumi.append(pieliekamais)
+    
+    return render_template("uni.html", uni = nosaukumi)
 
 if __name__ == '__main__':
     app.run(port=5000)
+
