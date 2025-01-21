64110ef860b856c79b0a4d8ca8ee96ffca15f2d4
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2025-01-21 12:27:50 +0200

    MACHINELEARNING

---
+++ MachineLearning/datu_apstrade.py
@@ -0,0 +1,26 @@
+import pandas as pd #failu apstrādei
+import matplotlib.pyplot as plt # grafiki
+import seaborn as sb #vizualizācija
+
+sb.set_style('whitegrid')
+plt.rcParams['figure.figsize']=(15,10)
+
+def heat_map(datne): #korelācijas sūdi
+    datu_fails = pd.read_csv(datne).select_dtypes('number')
+    sb.heatmap(datu_fails.corr(), annot=True, cmap='magma')
+    plt.show()
+    return
+
+def distribution(datne, kolonna):
+        datu_fails = pd.read_csv(datne)
+        sb.histplot(datu_fails[kolonna], color='r')
+
+        plt.show()
+
+
+auto_imports = "MachineLearning/auto_imports.csv"
+auto_simple = "MachineLearning/auto_simple.csv"
+
+
+heat_map("MachineLearning/auto_imports.csv")
+#distribution(auto_simple, "Car")
