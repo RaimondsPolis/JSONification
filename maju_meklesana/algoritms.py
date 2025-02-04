be5d7c42c985319c2e35738f1a674bede843e59b
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2025-02-04 12:29:40 +0200

    asddawd
    adawd

--- maju_meklesana/algoritms.py
+++ maju_meklesana/algoritms.py
@@ -5,37 +5,53 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
-bildes = []
-label = []
 
 bilzu_adrese = "maju_meklesana/bildes/"
 
-for nosaukums in os.listdir(bilzu_adrese):
-    image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200), Image.Resampling.NEAREST)
-    bildes.append(np.array(image))
-    if "maja" in nosaukums:
-        label.append(1)
-    else:
-        label.append(0)
-
-
-#datu sakitļu salabošana
-bildes = np.array(bildes)
-label = np.array(label)
-
-bildes = bildes/255.0 #
-bildes = bildes.reshape(bildes.shape[0], -1)
-
-X_train, X_test, y_train, y_test = train_test_split(bildes, label, test_size=3)#JA TEST SIZE IR LIELĀKS PAR 1 TAD VIŅŠ SKATĀS NO TĀ CIK FAILI IR PIEEJAMI
-
-#print(bildes)
-
 modelis = RandomForestClassifier()
 
-modelis.fit(X_train, y_train) #fit = trennē datoru
+def modela_trenesana(modelis):
+    bildes = []
+    label = []
 
-rezultats = modelis.predict(X_test)
+    for nosaukums in os.listdir(bilzu_adrese):
+        image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200), Image.Resampling.NEAREST)
+        bildes.append(np.array(image))
+        if "maja" in nosaukums:
+            label.append(1)
+        else:
+            label.append(0)
+    #datu sakitļu salabošana
+    bildes = np.array(bildes)
+    label = np.array(label)
 
-precizitate = accuracy_score(y_test, rezultats)
+    bildes = bildes/255.0 #
+    bildes = bildes.reshape(bildes.shape[0], -1)
 
-print(precizitate)
+    X_train, X_test, y_train, y_test = train_test_split(bildes, label, test_size=3)#JA TEST SIZE IR LIELĀKS PAR 1 TAD VIŅŠ SKATĀS NO TĀ CIK FAILI IR PIEEJAMI
+
+    modelis.fit(X_train, y_train) #fit = trennē datoru
+
+    rezultats = modelis.predict(X_test)
+
+    precizitate = accuracy_score(y_test, rezultats)
+    print(precizitate)
+    return modelis
+
+def majas_noteiksana(bilde, modelis):
+    bilde = [Image.open(bilde).resize((200,200), Image.Resampling.NEAREST)]
+    bilde = np.array(bilde) 
+    bilde = bilde/255.0
+    bilde = bilde.reshape(bilde.shape[0], -1)
+    rezultats = modelis.predict(bilde)
+    if rezultats[0] ==1:
+        rezultats = "ir maja"
+    if rezultats[0] ==0:
+        rezultats = "nav maja"
+    print(f"rezultats: {rezultats}")
+    return rezultats
+
+modelis = modela_trenesana(modelis)
+print("__________________________________________")
+majas_noteiksana("maju_meklesana/bildes/download1.jpg", modelis)
+print("__________________________________________")
